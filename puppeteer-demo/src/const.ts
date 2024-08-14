d72669b5d19febb9532e16597b9172b6bc33ea7b
houhongxu <hongxu.hou@goodwe.com>
2024-08-14 12:29:44 +0800

    feat: 移除puppeteer的demo

--- puppeteer-demo/src/const.ts
+++
@@ -1,7 +0,0 @@
-import path from 'path'
-
-export const DOWNLOAD_PATH = path.resolve('./download')
-export const LOG_PATH = path.resolve('./log')
-export const IMG_PATH = path.resolve('./img')
-
-export const DEFAULT_CONCURRENCY = 9
--- puppeteer-demo/src/test.ts
+++
@@ -1,8 +0,0 @@
-import { readJSON } from 'fs-extra'
-import path from 'path'
-import { LOG_PATH } from './const'
-
-readJSON(path.join(LOG_PATH, 'douyin', '小乔..json')).then(({ list }) => {
-  const res = list.map((i: any) => i.desc)
-  console.log(res)
-})
